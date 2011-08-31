472e50845f1419e1f256b5da6b6466d65c2dd8c2
francisco <francisco@francisco.(none)>
2011-08-31 16:26:17 +0100

    Revert "ARM: 7008/1: alignment: Make SIGBUS sent to userspace POSIXly correct"
    This reverts commit ac9f1e175005dbb2e43ae715eede58dff0a95c78.

--- arch/arm/mm/alignment.c
+++ arch/arm/mm/alignment.c
@@ -22,7 +22,6 @@
 #include <linux/sched.h>
 #include <linux/uaccess.h>
 
-#include <asm/system.h>
 #include <asm/unaligned.h>
 
 #include "fault.h"
@@ -912,16 +911,9 @@ do_alignment(unsigned long addr, unsigned int fsr, struct pt_regs *regs)
 	if (ai_usermode & UM_FIXUP)
 		goto fixup;
 
-	if (ai_usermode & UM_SIGNAL) {
-		siginfo_t si;
-
-		si.si_signo = SIGBUS;
-		si.si_errno = 0;
-		si.si_code = BUS_ADRALN;
-		si.si_addr = (void __user *)addr;
-
-		force_sig_info(si.si_signo, &si, current);
-	} else {
+	if (ai_usermode & UM_SIGNAL)
+		force_sig(SIGBUS, current);
+	else {
 		/*
 		 * We're about to disable the alignment trap and return to
 		 * user space.  But if an interrupt occurs before actually
